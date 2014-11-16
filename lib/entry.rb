f5ddff03f3eb6e841e3534f1a9345cb020c1dd9a
Bryce Holcomb <bryce_holcomb@outlook.com>
2014-11-15 19:31:56 -0700

    create finder and entries

--- lib/entry.rb
+++ lib/entry.rb
@@ -1,10 +1,23 @@
 class Entry
-  attr_reader :first_name, :last_name, :phone_number
+  attr_reader :first_name,
+              :last_name,
+              :name,
+              :email_address,
+              :phone_number,
+              :address,
+              :city,
+              :state,
+              :zip_code
 
   def initialize(data)
     @first_name = data[:first_name]
     @last_name = data[:last_name]
+    @email_address = data[:email_address]
     @phone_number = data[:phone_number]
+    @address = data[:address]
+    @city = data[:city]
+    @state = data[:state]
+    @zip_code = data[:zip_code]
   end
 
   def name
--- test/entry_test.rb
+++ test/entry_test.rb
@@ -4,17 +4,27 @@ require 'minitest/pride'
 require_relative '../lib/entry'
 
 class EntryTest < Minitest::Test
-  def test_entry_attributes
+  def test_it_has_attributes
     data = {
       first_name: 'John',
       last_name: 'Taylor',
-      phone_number: '111.111.1111'
+      email_address: 'johntaylor@gmail.com',
+      phone_number: '(704) 346 3455',
+      address: '1500 Blake Street',
+      city: 'Denver',
+      state: 'CO',
+      zip_code: '80211'
     }
     entry = Entry.new(data)
 
-    assert_equal 'Alice', entry.first_name
-    assert_equal 'Smith', entry.last_name
-    assert_equal 'Alice Smith', entry.name
-    assert_equal '111.111.1111', entry.phone_number
+    assert_equal 'John', entry.first_name
+    assert_equal 'Taylor', entry.last_name
+    assert_equal 'John Taylor', entry.name
+    assert_equal 'johntaylor@gmail.com', entry.email_address
+    assert_equal '(704) 346 3455', entry.phone_number
+    assert_equal '1500 Blake Street', entry.address
+    assert_equal 'Denver', entry.city
+    assert_equal 'CO', entry.state
+    assert_equal '80211', entry.zip_code
   end
 end
