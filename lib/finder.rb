2b826d958dacf89efaf781a50e4c963d1728ee0d
Laura Whalin <laurawhalin@gmail.com>
2014-11-15 20:27:09 -0700

    completed the bare minimum tests for assessment in finder: first name, last name, state, and city.

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,11 +1,8 @@
 require 'csv'
 require_relative 'entry'
-require 'pry'
-binding.pry
 
 class Finder
-  def
-
+  attr_reader :entries
   def self.load_entries(directory)
     file = File.join(directory, 'event_attendees.csv')
     data = CSV.open(file, headers: true)
@@ -27,4 +24,23 @@ class Finder
     new(rows)
   end
 
+  def initialize(entries)
+   @entries = entries
+  end
+
+  def find_by_first_name(string)
+    entries.select { |entry| entry.first_name == string }
+  end
+
+  def find_by_last_name(string)
+    entries.select { |entry| entry.last_name == string }
+  end
+
+  def find_by_city(string)
+    entries.select { |entry| entry.city == string }
+  end
+
+  def find_by_state(string)
+    entries.select { |entry| entry.state == string }
+  end
 end
