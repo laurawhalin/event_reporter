4608a10c6737ea530f09cb39bd9997fb9b4ca422
Laura Whalin <laurawhalin@gmail.com>
2014-11-15 19:54:51 -0700

    updated finder_test with all the tests.

--- test/finder_test.rb
+++ test/finder_test.rb
@@ -29,14 +29,74 @@ class FinderTest < Minitest::Test
   end
 
   def test_find_by_last_name
+    entries = [
+      { first_name: 'John', last_name: 'Taylor', phone_number: '111.111.1111' },
+      { first_name: 'Bob', last_name: 'Smith', phone_number: '222.222.2222' },
+      { first_name: 'Cindy', last_name: 'Johnson', phone_number: '333.333.3333' }
+    ].map { |row| Entry.new(row) }
 
+    finder = Finder.new(entries)
+    entries = finder.find_by_last_name('Taylor')
+    assert_equal 1, entries.length
+  end
+
+  def test_find_by_last_name_with_multiple_returns
+    entries = [
+      { first_name: 'John', last_name: 'Taylor', phone_number: '111.111.1111' },
+      { first_name: 'Bob', last_name: 'Taylor', phone_number: '222.222.2222' },
+      { first_name: 'Cindy', last_name: 'Johnson', phone_number: '333.333.3333' }
+    ].map { |row| Entry.new(row) }
+
+    finder = Finder.new(entries)
+    entries = finder.find_by_last_name('Taylor')
+    assert_equal 2, entries.length
   end
 
   def test_find_by_city
+    entries = [
+      { first_name: 'John', last_name: 'Taylor', phone_number: '111.111.1111', city: 'Denver' },
+      { first_name: 'Bob', last_name: 'Smith', phone_number: '222.222.2222', city: 'Tuscon' },
+      { first_name: 'Cindy', last_name: 'Johnson', phone_number: '333.333.3333', city: 'New York' }
+    ].map { |row| Entry.new(row) }
 
+    finder = Finder.new(entries)
+    entries = finder.find_by_city('Denver')
+    assert_equal 1, entries.length
   end
 
-  def test_find_by_zip_code
+  def test_find_by_city_with_multiple_returns
+    entries = [
+    { first_name: 'John', last_name: 'Taylor', phone_number: '111.111.1111', city: 'Denver' },
+    { first_name: 'Bob', last_name: 'Smith', phone_number: '222.222.2222', city: 'Denver' },
+    { first_name: 'Cindy', last_name: 'Johnson', phone_number: '333.333.3333', city: 'New York' }
+    ].map { |row| Entry.new(row) }
 
+    finder = Finder.new(entries)
+    entries = finder.find_by_city('Denver')
+    assert_equal 2, entries.length
+  end
+
+  def test_find_by_state
+    entries = [
+      { first_name: 'John', last_name: 'Taylor', state: 'CO' },
+      { first_name: 'Bob', last_name: 'Smith', state: 'MD' },
+      { first_name: 'Cindy', last_name: 'Johnson', state: 'NY' }
+    ].map { |row| Entry.new(row) }
+
+    finder = Finder.new(entries)
+    entries = finder.find_by_state('CO')
+    assert_equal 1, entries.length
+  end
+
+  def test_find_by_state_with_multiple_returns
+    entries = [
+      { first_name: 'John', last_name: 'Taylor', state: 'CO' },
+      { first_name: 'Bob', last_name: 'Smith', state: 'CO' },
+      { first_name: 'Cindy', last_name: 'Johnson', state: 'NY' }
+    ].map { |row| Entry.new(row) }
+
+    finder = Finder.new(entries)
+    entries = finder.find_by_state('CO')
+    assert_equal 2, entries.length
   end
 end
