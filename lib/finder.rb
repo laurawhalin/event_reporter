b27db0e48d7558e88549d81aadff932c426de15a
Bryce Holcomb <bryce_holcomb@outlook.com>
2014-11-19 14:36:54 -0700

    converted existing headers into syms and downcase

--- lib/finder.rb
+++ lib/finder.rb
@@ -6,19 +6,7 @@ class Finder
 
   def self.load_entries(name = 'event_attendees.csv')
     file = File.join("./data", name)
-    data = CSV.open(
-          file,
-          :headers => [ :entry,
-                        :reg_date,
-                        :first_name,
-                        :last_name,
-                        :email_address,
-                        :phone_number,
-                        :address,
-                        :city,
-                        :state,
-                        :zip_code]
-      )
+    data = CSV.open(file, headers: true, :header_converters => :symbol.downcase)
 
     rows = data.map do |row|
       Entry.new(row)
